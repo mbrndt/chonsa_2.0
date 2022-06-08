c674c5a2a6feae8f9dd595cf48451e50a3eb2ad5
Mareike <mareike.brandt@hotmail.de>
2022-06-08 15:49:41 +0900

    feat: set up apollo and spinner animation

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const app = express();
 
 //
 connectDB();
 
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({

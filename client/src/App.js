6d2bfcec1014c36dce38e3ba0005294ad7fc6061
Mareike <mareike.brandt@hotmail.de>
2022-06-13 15:07:44 +0900

    feat: data can now be deleted from the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,21 @@ import Header from "./components/Header";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Clients from "./components/Clients";
 
+const cache = new InMemoryCache({
+  typePolicy: {
+    Query: {
+      fields: {
+        clients: {
+          merge: (existing, incoming) => incoming,
+        },
+      },
+      projects: {
+        merge: (existing, incoming) => incoming,
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
   cache: new InMemoryCache(),

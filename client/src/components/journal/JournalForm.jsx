7b2bb74c77d839eac6ab6ed7bf33662599f71f47
Mareike <mareike.brandt@hotmail.de>
2022-07-02 19:14:23 +0900

    feat: now able to save journal entries

--- client/src/components/journal/JournalForm.jsx
+++ client/src/components/journal/JournalForm.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import { useState } from "react";
 import { FaPen } from "react-icons/fa";
+import { ADD_JOURNAL } from "../../mutations/journalMutations";
+import { GET_JOURNAL } from "../../queries/journalQueries";
+import { useMutation } from "@apollo/client";
+
 import "./Journal.css";
 
 function JournalForm() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const [addJournal] = useMutation(ADD_JOURNAL, {
+    variables: { title, content },
+    update(cache, { data: { addJournal } }) {
+      const { journals } = cache.readQuery({ query: GET_JOURNAL });
+      cache.writeQuery({
+        query: GET_JOURNAL,
+        data: { journals: journals.concat([addJournal]) },
+      });
+    },
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (title === "" || content === "") {
+      alert("Please fill in all fields");
+    }
+    addJournal(title, content);
+    setTitle("");
+    setContent("");
+  };
+
   return (
     <div className="journalForm">
       <h1>Journal Form</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="">
           <label htmlFor="title">Title</label>
           <input
@@ -14,6 +43,8 @@ function JournalForm() {
             className="formControl"
             id="titleTextArea"
             placeholder="new title ..."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="">
@@ -22,6 +53,8 @@ function JournalForm() {
             className="formControl"
             id="contentTextArea"
             placeholder="please write here ..."
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
           ></textarea>
         </div>
         <button type="submit">

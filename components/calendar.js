d3232fb16154666ff535e692c4057b49110281fe
Mareike <mareike.brandt@hotmail.de>
2022-10-16 20:29:16 +0900

    feat: added calendar

--- components/calendar.js
+++ components/calendar.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { useState } from "react";
 
-function calendar() {
+export default function CalendarCom() {
+  const [value, onChange] = useState(new Date());
+
   return (
-    <div className="bg-lilacBg">
-      <h1>here goes the calendar</h1>
+    <div className="bg-lilacBg items-center justify-center ">
+      <h1>calendar </h1>
+      <div className="justify-center flex bg-lilacBg">
+        <Calendar onChange={onChange} value={value} />
+      </div>
     </div>
   );
 }
-
-export default calendar;
